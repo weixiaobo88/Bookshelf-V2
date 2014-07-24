73fd3d20871c93b75bf107f0400b0e5e1a164ce7
Zhaoyu Wei <weixiaobo88@gmail.com>
2014-07-24 18:32:04 +0800

    zy: init bookshelf project with helloworld example

---
+++ src/main/java/com/tw/bookshelf/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.tw.bookshelf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by twer on 7/24/14.
+ */
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/hello")
+    public String hello(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "helloworld";
+    }
+
+}
